4f847588a8214eb00bf8eb9895d09719c41bfc51
Manuel <hello@m-k.io>
2019-08-19 11:43:20 +0200

    feat: logging

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ const app = express();
 
 app.use(express.static("public"));
 
+console.log("secret", process.env.TELEGRAM_WEBHOOK);
+console.log("secret", process.env.TELEGRAM_SECRET_PATH);
+
 app.use(Telegram().webhookCallback(process.env.TELEGRAM_SECRET_PATH));
 
 app.listen(PORT, () => {
